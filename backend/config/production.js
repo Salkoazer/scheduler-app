c8ee8c7b5e60d4b45675bc29bc7d3b44292f47da
Salkoazer <wilsonveterano@gmail.com>
2025-08-13 16:56:10 +0100

    Replaced hardcoded secrets with environment variables and added .env file

--- backend/config/production.js
+++ backend/config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    mongoUri: "mongodb+srv://veterano:wilson17@cluster0.ocdjg.mongodb.net/schedule-app-database?retryWrites=true&w=majority&appName=Cluster0",
-    jwtSecret: "3732df64ca5d86bf5edfe0cf36cc9dd15243abbd83a2434fc52f429b80e60e46fe14664096590d7144582f43bdc68815ab72d428729f52e9cb2e046d4beeb7a2",
+    mongoUri: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
     // Add other production-specific configuration
 };
